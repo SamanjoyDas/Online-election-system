a3fcb7691ab09f9863dbfa5199964903d9612236
SamanjoyDas <samanjoydas964@gmail.com>
2022-09-16 11:38:43 +0530

    updated email

---
+++ Votingpage.js
@@ -0,0 +1,5 @@
+function redirect(){
+    alert('Thank you for voting!\n Your vote has been saved');
+
+    setTimeout(()=>{window.location.replace('index.html');}, 750);
+}
--- index.js
+++ index.js
@@ -2,10 +2,28 @@ setInterval(changeimgs, 2000) ;
 
 function changeimgs(){
     
-    let index = Math.floor(Math.random() * 6);
+    let index = Math.floor(Math.random() * 5);
     
     let imgpath = "Images/" ;
-    const images = ["img3.jpg" , "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img9.jpg"] ;
+    const images = ["img3.jpg" , "img4.jpg", "img5.jpg", "img7.jpg", "img9.jpg"] ;
 
     document.getElementById('storyimg').src = imgpath + images[index] ;
+}
+
+function ValidateEmail(inputText, inputPass){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(inputText.value.match(mailformat))
+    {
+        if(inputPass.value.length >= 8){
+        alert("Your Email & Password has been verified successfully");
+         }
+         else{
+            alert("Password should be of Minimum 8 characters");
+         }
+    }
+    else
+    {
+        alert("Try again");
+    }
 }
